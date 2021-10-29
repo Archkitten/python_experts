77a1a42af99b9229838167743440b34fb8c6211e
Arch Huang <ArchKitten05@gmail.com>
2021-10-28 18:08:34 -0700

    Terminal Commit 5 - Yeah this system sucks I'll change it

--- main.py
+++ main.py
@@ -247,32 +247,28 @@ def rgb():
 
 
 
+# Terminal G1, Step 1.
 
-
-@app.route('/games/terminal/terminalG1', methods=['GET', 'POST'])
+@app.route('/games/terminal/', methods=['GET', 'POST'])
 def terminalG1():
     # submit button has been pushed
     if request.form:
         commandInputPY = request.form.get("commandInput")
         if commandInputPY == "echo":  # input field has content
             return render_template("/terminal/terminalG1.html",
-                                   commandOutput1=commandInputPY,
-                                   commandOutput2=commandInputPY,
-                                   commandOutput3=commandInputPY,
-                                   commandOutput4=commandInputPY)
+                                   commandOutput1=commandInputPY)
         elif commandInputPY == "viewport":
             return render_template("/terminal/terminalG1.html",
-                                   commandOutput1="G1: Open, In Use",
+                                   commandOutput1="G1: Open, Password Protected, In Use",
                                    commandOutput2="G2: Open",
-                                   commandOutput3="G3: Closed",
-                                   commandOutput4="G4: Closed")
+                                   commandOutput3="G3: Closed, Password Protected",
+                                   commandOutput4="G4: Open, Password Protected")
         elif commandInputPY == "connect G1":
             return render_template("/terminal/terminalG1.html",
                                    commandOutput1="Connection Terminated:",
                                    commandOutput2="Port In Use")
         elif commandInputPY == "connect G2":
-            return render_template("/terminal/terminalG1.html",
-                                   commandOutput1="ㅤ")
+            return render_template("/terminal/terminalG2.html")
         elif commandInputPY == "connect G3" or commandInputPY == "connect G4":
             return render_template("/terminal/terminalG1.html",
                                    commandOutput1="Connection Terminated:",
@@ -285,7 +281,7 @@ def terminalG1():
                                    commandOutput1="Hello Agent G1. Your task is to secure this terminal and prevent information leaks.",
                                    commandOutput2="At the same time, if you can find any information from competitors, don't be afraid to snoop around and record anything you find.",
                                    commandOutput3="In case you forget how to operate this terminal, a list of commands has been provided below. Good luck.",
-                                   commandOutput4="echo - Tests terminal functionality.")
+                                   commandOutput4='P.S. Your password is "password". Keep it secure.')
         elif commandInputPY == "run README.txt":
             return render_template("/terminal/terminalG1.html",
                                    commandOutput1="Error: Non-executable File")
@@ -295,6 +291,51 @@ def terminalG1():
     return render_template("/terminal/terminalG1.html",
                            commandOutput1="Awaiting Input...")
 
+# Terminal G2, Step 2.
+
+@app.route('/games/terminal/terminalG2/', methods=['GET', 'POST'])
+def terminalG2():
+    # submit button has been pushed
+    if request.form:
+        commandInputPY = request.form.get("commandInput")
+        if commandInputPY == "echo":  # input field has content
+            return render_template("/terminal/terminalG2.html",
+                                   commandOutput1=commandInputPY)
+        elif commandInputPY == "viewport":
+            return render_template("/terminal/terminalG2.html",
+                                   commandOutput1="G1: Open, Password Protected",
+                                   commandOutput2="G2: Open, In Use",
+                                   commandOutput3="G3: Closed",
+                                   commandOutput4="G4: Open, Password Protected")
+        elif commandInputPY == "connect G1":
+            return render_template("/terminal/terminalG2.html",
+                                   commandOutput1="Connection Terminated:",
+                                   commandOutput2="Port In Use")
+        elif commandInputPY == "connect G2":
+            return render_template("/terminal/terminalG2.html",
+                                   commandOutput1="ㅤ")
+        elif commandInputPY == "connect G3" or commandInputPY == "connect G4":
+            return render_template("/terminal/terminalG2.html",
+                                   commandOutput1="Connection Terminated:",
+                                   commandOutput2="Request Timeout")
+        elif commandInputPY == "scan":
+            return render_template("/terminal/terminalG2.html",
+                                   commandOutput1="2019-04-17 passwords.txt",
+                                   commandOutput2="2019-04-21 rickroll.mp3")
+        elif commandInputPY == "display passwords.txt":
+            return render_template("/terminal/terminalG2.html",
+                                   commandOutput1="password",
+                                   commandOutput2="N/A",
+                                   commandOutput3="yP4x23k",
+                                   commandOutput4="f2Q12d")
+        elif commandInputPY == "run README.txt":
+            return render_template("/terminal/terminalG2.html",
+                                   commandOutput1="Error: Non-executable File")
+        else:
+            return render_template("/terminal/terminalG2.html",
+                                   commandOutput1="Unknown command.")
+    return render_template("/terminal/terminalG2.html",
+                           commandOutput1="Awaiting Input...")
 
 
 
